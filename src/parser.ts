1fefb941fb96843c931858f41538c5dccbc6a2df
Fansy <418536538@qq.com>
2021-09-02 23:59:43 +0800

    feat: add parser travel for traveller

--- src/parser.ts
+++ src/parser.ts
@@ -1,6 +1,6 @@
-import postcss, { Root, Syntax } from 'postcss';
+import postcss, { AtRule, Comment, Declaration, Root, Rule, Syntax } from 'postcss';
 import lessSyntax from 'postcss-less';
-import { ParserType, Traveller } from './interfaces';
+import { ParserType, Traveller, Output } from './interfaces';
 
 export const parse = async (content: string, type: ParserType) => {
     let syntax: Syntax;
@@ -16,8 +16,36 @@ export const parse = async (content: string, type: ParserType) => {
     return ast.root;
 };
 
-export const travel = (ast: Root, traveller: Traveller) => {
-    const { onComplete } = traveller();
+export type ASTNode = Root | AtRule | Rule;
 
-    return onComplete();
+export const travel = (ast: ASTNode, traveller: Traveller): Output => {
+    for (const node of ast.nodes) {
+        if (node instanceof AtRule) {
+            traveller.onAtRule?.(node);
+            continue;
+        }
+
+        if (node instanceof Comment) {
+            traveller.onComment?.(node);
+            continue;
+        }
+
+        if (node instanceof Rule) {
+            traveller.onRule?.(node);
+            continue;
+        }
+
+        if (node instanceof Declaration) {
+            traveller.onDeclaration?.(node);
+            continue;
+        }
+
+        if ('nodes' in node) {
+            travel(node as ASTNode, traveller);
+        }
+    }
+
+    return {
+        variables: traveller.result,
+    };
 };

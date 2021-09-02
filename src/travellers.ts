09012c5524b65818553b7ae0a75d876cceaf714c
Fansy <418536538@qq.com>
2021-09-02 23:57:35 +0800

    chore: remove unless file

--- src/travellers.ts
+++
@@ -1,9 +0,0 @@
-import { Traveller } from './interfaces';
-
-export const less: Traveller = () => {
-    const variables: Record<string, string> = {};
-
-    return {
-        onComplete: () => variables,
-    };
-};

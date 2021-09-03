2da31c5e86152551f3eb86abcbf89b3844aa1c5f
Fansy <418536538@qq.com>
2021-09-04 00:26:20 +0800

    test: add `getScss` test util

--- __tests__/utils.ts
+++ __tests__/utils.ts
@@ -4,3 +4,5 @@ import * as path from 'path';
 export const getAsset = (fileName: string) => fs.readFileSync(path.resolve(__dirname, './assets', fileName), 'utf-8');
 
 export const getLess = () => getAsset('normal.less');
+
+export const getScss = () => getAsset('normal.scss');
